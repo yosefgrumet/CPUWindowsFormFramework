881951e2b0aa23c60787b5fb2446b4344e3098bf
yosefgrumet <yosefgrumet@gmail.com>
2024-09-21 22:38:54 -0400

    hw 23

--- WindowsFormsUtility.cs
+++ WindowsFormsUtility.cs
@@ -16,7 +16,7 @@ namespace CPUWindowsFormFramework
             lst.DisplayMember = lst.Name.Substring(3);
             lst.DataBindings.Add("SelectedValue", targetdt, lst.ValueMember, false, DataSourceUpdateMode.OnPropertyChanged);
         }
-        public static void SetControlBinding(Control ctrl, DataTable dt)
+        public static void SetControlBinding(Control ctrl, BindingSource bindsource)
         {
             string propertyname = "";
             string controlname = ctrl.Name.ToLower();
@@ -37,7 +37,7 @@ namespace CPUWindowsFormFramework
 
             if (propertyname != "" && columnname != "")
             {
-                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+                ctrl.DataBindings.Add(propertyname, bindsource, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
             }
         }
         public static void FormatGridForSearchResults(DataGridView grid)
